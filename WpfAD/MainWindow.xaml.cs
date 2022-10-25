58262cdb7414dd0d0898691ba20cb33df52419a2
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-25 15:56:22 +0200

    Nästa steg är att fixa Listview så att kontakerna syns på listan

--- WpfAD/MainWindow.xaml.cs
+++ WpfAD/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAD.Models;
 
 namespace WpfAD
 {
@@ -20,14 +22,24 @@ namespace WpfAD
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<ContactPerson> contacts; // Det sk finnas en lista
         public MainWindow()
         {
             InitializeComponent();
+            contacts = new ObservableCollection<ContactPerson>(); //Skapar en instanciering av lista
         }
 
         private void tbn_Add_Click(object sender, RoutedEventArgs e) //Lägg till metod
         {
-
-        }
+            contacts.Add(new ContactPerson
+            { 
+                FirstName = tb_FirstName.Text,
+                LastName = tb_LastName.Text,
+                Email = tb_Email.Text,
+                StreetName = tb_StreetName.Text,
+                PostalCode = tb_PostalCode.Text,
+                City = tb_City.Text,
+            });  // Kontakter stoppas in direkt i listan utan behov till variabler
+        } 
     }
 }
