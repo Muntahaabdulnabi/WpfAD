d10d65afcf25f8e903bb30c566fa014b0ada04ef
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-25 19:07:34 +0200

    En ny Service (FileManager) added för läsa och skriva

---
+++ WpfAD/services/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAD.services
+{
+    internal interface IFileManager
+    {
+        public void Save(string filePath, string content);
+        public string Read(string filePath);
+
+    }
+    internal class FileManager : IFileManager
+    {
+        public string Read(string filePath)
+        {
+            using var sr = new StreamReader(filePath);
+            return sr.ReadToEnd();
+        }
+
+        public void Save(string filePath, string content)
+        {
+            using var sw = new StreamWriter(filePath);
+            sw.Write(content);
+        }
+    }
+}
